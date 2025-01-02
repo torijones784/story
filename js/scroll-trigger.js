b04aacdeab1eda91a61ef73bad887f5b7c62585f
torijones784 <73712132+torijones784@users.noreply.github.com>
2025-01-02 12:55:44 -0600

    Update scroll-trigger.js

--- js/scroll-trigger.js
+++ js/scroll-trigger.js
@@ -6,7 +6,7 @@ function initScrollTrigger() {
     let hasReachedBottom = false;  
     let fifthTriggerActivated = false;
     let pageLoadTime = Date.now();
-    const MINIMUM_TIME = 30; // Change back
+    const MINIMUM_TIME = 30;
 
     let lastViewportHeight = window.innerHeight;
     
