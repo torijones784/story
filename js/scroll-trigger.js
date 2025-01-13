b1fb5e275a6e556bfbcc4825e585b7de79e62acf
torijones784 <73712132+torijones784@users.noreply.github.com>
2025-01-13 11:55:46 -0600

    increase opacity

--- js/scroll-trigger.js
+++ js/scroll-trigger.js
@@ -103,12 +103,12 @@ function initScrollTrigger() {
         
         function handleTextChange() {
             points.forEach(point => {
-                point.element.style.opacity = '0.15';
-                point.mirrorElement.style.opacity = '0.15';
+                point.element.style.opacity = '0.3';
+                point.mirrorElement.style.opacity = '0.3';
                 
                 setTimeout(() => {
-                    point.element.style.opacity = '0.05';
-                    point.mirrorElement.style.opacity = '0.05';
+                    point.element.style.opacity = '0.15';
+                    point.mirrorElement.style.opacity = '0.15';
                 }, 2000);
             });
         }
