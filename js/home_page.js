71dd5d5232ba2ea788d994b40e18f4b42dac9911
torijones784 <73712132+torijones784@users.noreply.github.com>
2025-02-18 14:33:10 -0600

    reveal word one

--- js/home_page.js
+++ js/home_page.js
@@ -1,7 +1,15 @@
 let word_one = document.getElementById('word_one');
+let word_two = document.getElementById('word_two');
+let word_three = document.getElementById('word_three');
 
-
+function headlineReveal() {
+    setTimeout(() => {
+        word_one.classList.remove('hidden');
+        word_one.classList.add('visible');
+    }, 1000)
+} 
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log(word_one)
+    headlineReveal();
 });
